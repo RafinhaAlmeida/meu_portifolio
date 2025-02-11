307940396bf904ab7f82268b7911fb4c7ff7b1a8
RafinhaAlmeida Rodrigues <s0l1d3r1c3@gmail.com>
2025-02-11 01:39:53 +0000

    terceiro

--- src/pages/Capa.jsx
+++ src/pages/Capa.jsx
@@ -1,28 +1,5 @@
-import { TitleContainer, SaibaMaisContainer, FundoContainer, EuContainer } from "./Capa.styles";
-
-import MinhaCara from "../images/MinhaCara"
-import FundoCapa from "../images/FundoCapa"
-
-
-
 export function Capa() {
-
   return (
-    <div>
-      <FundoContainer>
-        <img src={FundoCapa} alt="FundoCapa" />
-      </FundoContainer>
-      <EuContainer>
-        <img src={MinhaCara} alt="MinhaCara" />
-      </EuContainer>
-      <TitleContainer>
-          Meu Portfólio como técnico em informática 
-        </TitleContainer>
-
-      <SaibaMaisContainer>
-        <button>Saiba mais...</button>
-      </SaibaMaisContainer>
-    </div>
-  
-)
+    <div>Capa</div>
+  )
 }
